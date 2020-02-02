d844221f937f6ca1cb630faed9cbea0b57c890be
Andrew Dimery <Andrewbrewaskew@gmail.com>
2020-02-02 17:14:43 -0600

    Add hello_db.py

---
+++ hello_db.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+# Creates or opens connection to db file
+
+conn = sqlite3.connect('first_db.sqlite')
+
+# Create a table
+conn.excite('create table phone  (brand text, version interger)')
+
+# Add some data 
+conn.execute('insert into phones values ("Android", 5)')
+conn.execute('insert into phones values ("iPhone", 6)')
+
+conn.commit()
+
+
+# Excute a query. excute() returns a cusor
+# Can use the sursor in a loop to read each row in turn.
+for row in conn.execute('select * from phones'):
+    print(row)
+
+conn.execute('drop table phones') # Delete table
+
+conn.commit() # Ask the database to save changes - dont forget!
+
+conn.close() # And close connection. 
+
